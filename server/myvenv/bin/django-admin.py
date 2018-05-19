78d3418edfcf9463bcf5d3c94a0a4c472c511d8d
sgc109 <sgc109109@gmail.com>
2018-05-20 01:03:06 +0900

    Remove the now ignored directory "some-directory"

--- server/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/sgc109/Documents/Algorithm/midas_practice/server/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
