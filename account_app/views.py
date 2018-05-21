b3beb1307587c556ee1556f232afd1f7ff43c6a2
sgc109 <sgc109109@gmail.com>
2018-05-22 02:01:25 +0900

    프사 변경 코드 완성

--- account_app/views.py
+++ account_app/views.py
@@ -8,6 +8,12 @@ from django.core import serializers
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+def error_response():
+    return JsonResponse({},status=400)
+
+def success_response():
+    return JsonResponse({},status=200)
+
 def add_user(request):
     id = request.GET.get('id', '')
     pw = request.GET.get('pw', '')
@@ -17,40 +23,47 @@ def add_user(request):
         profile = Profile(user=new_user, name='test_name', profile_image='', comment='')
         profile.save()
     except IntegrityError as e:
-        return JsonResponse({}, status=400)
+        return error_response()
         #return HttpResponse(e.__cause__)
 
-    return JsonResponse({}, status=200)
+    return success_response()
 
 def remove_user(request):
     id = request.GET.get('id', '')
     pw = request.GET.get('pw', '')
     query_set = User.objects.all().filter(username=id, password=pw)
     cnt = query_set.count()
-    if cnt > 0:
-        if cnt > 1:
-            return JsonResponse({}, status=400)
-        query_set.delete()
-        return JsonResponse({}, status=200)
-    else:
-        return JsonResponse({}, status=400)
+    try:
+        if cnt > 0:
+            if cnt > 1:
+                return error_response()
+            query_set.delete()
+            return success_response()
+        else:
+            return error_response()
+    except:
+        return error_response()
 
 def login(request):
     id = request.GET.get('id', '')
     pw = request.GET.get('pw', '')
     if User.objects.all().filter(username=id, password=pw).count() == 0:
-        return JsonResponse({}, status=400)
+        return error_response()
 
     query_set = User.objects.all().filter(username=id)
     cnt = query_set.count()
-    if cnt > 0:
-        if cnt > 1:
-            return JsonResponse({}, status=400)
-        user = query_set[0]
-        profile = Profile.objects.all().filter(user=user)[0]
-        profile_serializer = ProfileSerializer(profile, context={'request': request})
-        return JsonResponse(profile_serializer.data, status=200)
-    return JsonResponse({}, status=400)
+    try:
+        if cnt > 0:
+            if cnt > 1:
+                return error_response()
+            user = query_set[0]
+            profile = Profile.objects.all().filter(user=user)[0]
+            profile_serializer = ProfileSerializer(profile, context={'request': request})
+            return JsonResponse(profile_serializer.data, status=200)
+    except:
+        return error_response()
+
+    return error_response()
 
 @csrf_exempt
 def change_profile_image(request):
@@ -58,13 +71,16 @@ def change_profile_image(request):
     pw = request.POST.get('pw', '')
     query_set = User.objects.all().filter(username=id, password=pw)
     cnt = query_set.count()
-    if cnt > 0:
-        if cnt > 1:
-            return JsonResponse({}, status=400)
-        user = query_set[0]
-        profile = Profile.objects.all().filter(user=user)[0]
-        profile.profile_image = request.FILES['file']
-        profile.save()
-        return JsonResponse({}, status=200)
-    else:
-        return JsonResponse({}, status=400)
+    try:
+        if cnt > 0:
+            if cnt > 1:
+                return error_response()
+            user = query_set[0]
+            profile = Profile.objects.all().filter(user=user)[0]
+            profile.profile_image = request.FILES['file']
+            profile.save()
+            return success_response()
+        else:
+            return error_response()
+    except:
+        return error_response()
