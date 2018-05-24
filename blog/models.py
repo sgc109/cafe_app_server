7a66cc3964b081c31aa8084dfeedfd1033cd8548
sgc109 <sgc109109@gmail.com>
2018-05-25 01:57:09 +0900

    버그 수정

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from account_app.models import Profile
+from datetime import date
+import uuid
 import os
 
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join('photos/%Y/%m/%d/', filename)
+    return os.path.join(date.today().strftime('photos/%Y/%m/%d/'), filename)
 
 class Post(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
