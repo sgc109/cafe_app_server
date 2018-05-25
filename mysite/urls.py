b6b1e33644e33d3823ddf51de055c1841385e3f0
sgc109 <sgc109109@gmail.com>
2018-05-25 14:55:34 +0900

    asdf

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,24 +1,10 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 import account_app.views
 import blog.views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -31,6 +17,7 @@ urlpatterns = [
     url(r'^upload_post/', blog.views.upload_post, name = 'upload_post'),
     url(r'^delete_post/', blog.views.delete_post, name = 'delete_post'),
     url(r'^get_posts/', blog.views.get_posts, name = 'get_posts'),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
